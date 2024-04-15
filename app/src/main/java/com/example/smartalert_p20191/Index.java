c9f47327c537d0fd74c568a43f0383a36b97a749
yorgos <tsakirakii@gmail.com>
2024-04-15 15:06:19 +0300

    Added Users1 activity - upload form design

--- app/src/main/java/com/example/smartalert_p20191/Index.java
+++ app/src/main/java/com/example/smartalert_p20191/Index.java
@@ -11,4 +11,7 @@ public class Index extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_index);
     }
+    // users --> ola ( leitourgies: LOG-IN kai SIGN-UP)
+    //employees -- hide first name, last name kai username ( mono LOG-IN. den mporei na ginei kainourgia eggrafh gia zhthmata asfaleias)
+    //
 }
