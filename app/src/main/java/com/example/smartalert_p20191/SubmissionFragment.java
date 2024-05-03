9d403b0ed8ec8856b74225d2076487efe2dc8494
yorgos <tsakirakii@gmail.com>
2024-05-03 21:04:18 +0300

    Added bottom navigation bar. Error: 'constant expression required'

---
+++ app/src/main/java/com/example/smartalert_p20191/SubmissionFragment.java
@@ -0,0 +1,16 @@
+package com.example.smartalert_p20191;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.fragment.app.Fragment;
+
+public class SubmissionFragment extends Fragment {
+    public SubmissionFragment(){
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_submission, container, false);
+    }
+}
--- app/src/main/java/com/example/smartalert_p20191/Users1Activity.java
+++ app/src/main/java/com/example/smartalert_p20191/Users1Activity.java
@@ -1,22 +1,62 @@
 package com.example.smartalert_p20191;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class Users1Activity extends AppCompatActivity {
-
+public class Users1Activity extends AppCompatActivity
+        implements BottomNavigationView
+        .OnNavigationItemSelectedListener {
+    BottomNavigationView bottomNavigationView;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.users1);
+        setContentView(R.layout.activity_main);
         Spinner dropdown = findViewById(R.id.spinner1);
         String[] items = new String[]{"Plummura", "Purkagia", "Seismos"};
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
         dropdown.setAdapter(adapter);
+        bottomNavigationView
+                = findViewById(R.id.bottomNavigationView);
+
+        bottomNavigationView
+                .setOnNavigationItemSelectedListener(this);
+        bottomNavigationView.setSelectedItemId(R.id.submission);
     }
+    SubmissionFragment submissionFragment = new SubmissionFragment();
+    StatisticsFragment statisticsFragment = new StatisticsFragment();
+    LanguageFragment languageFragment = new LanguageFragment();
+    @Override
+    public boolean
+    onNavigationItemSelected(@NonNull MenuItem item)
+    {
+        switch (item.getItemId()) {
+            case R.id.submission:
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.Fragment1, submissionFragment)
+                        .commit();
+                return true;
 
+            case R.id.statistics:
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.Fragment1, statisticsFragment)
+                        .commit();
+                return true;
 
+            case R.id.language:
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.Fragment1, languageFragment)
+                        .commit();
+                return true;
+        }
+        return false;
+    }
 }
