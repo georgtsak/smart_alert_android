5647ffd2c824c56c12a1023cbba50c6b179af283
yorgos <tsakirakii@gmail.com>
2024-05-22 16:24:56 +0300

    Updated bottom navigation bar. Error: 'constant expression required'

--- app/src/main/java/com/example/smartalert_p20191/LanguageFragment.java
+++ app/src/main/java/com/example/smartalert_p20191/LanguageFragment.java
@@ -4,14 +4,17 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
 import androidx.fragment.app.Fragment;
 
 public class LanguageFragment extends Fragment {
+
     public LanguageFragment(){
+        // require a empty public constructor
     }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
+        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_language, container, false);
     }
 }
--- app/src/main/java/com/example/smartalert_p20191/Users1Activity.java
+++ app/src/main/java/com/example/smartalert_p20191/Users1Activity.java
@@ -1,62 +1,17 @@
 package com.example.smartalert_p20191;
 
 import android.os.Bundle;
-import android.view.MenuItem;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
-import androidx.annotation.NonNull;
+
 import androidx.appcompat.app.AppCompatActivity;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class Users1Activity extends AppCompatActivity
-        implements BottomNavigationView
-        .OnNavigationItemSelectedListener {
-    BottomNavigationView bottomNavigationView;
+public class Users1Activity extends AppCompatActivity {
+
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Spinner dropdown = findViewById(R.id.spinner1);
-        String[] items = new String[]{"Plummura", "Purkagia", "Seismos"};
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
-        dropdown.setAdapter(adapter);
-        bottomNavigationView
-                = findViewById(R.id.bottomNavigationView);
+        setContentView(R.layout.users1);
 
-        bottomNavigationView
-                .setOnNavigationItemSelectedListener(this);
-        bottomNavigationView.setSelectedItemId(R.id.submission);
     }
-    SubmissionFragment submissionFragment = new SubmissionFragment();
-    StatisticsFragment statisticsFragment = new StatisticsFragment();
-    LanguageFragment languageFragment = new LanguageFragment();
-    @Override
-    public boolean
-    onNavigationItemSelected(@NonNull MenuItem item)
-    {
-        switch (item.getItemId()) {
-            case R.id.submission:
-                getSupportFragmentManager()
-                        .beginTransaction()
-                        .replace(R.id.Fragment1, submissionFragment)
-                        .commit();
-                return true;
 
-            case R.id.statistics:
-                getSupportFragmentManager()
-                        .beginTransaction()
-                        .replace(R.id.Fragment1, statisticsFragment)
-                        .commit();
-                return true;
 
-            case R.id.language:
-                getSupportFragmentManager()
-                        .beginTransaction()
-                        .replace(R.id.Fragment1, languageFragment)
-                        .commit();
-                return true;
-        }
-        return false;
-    }
 }
