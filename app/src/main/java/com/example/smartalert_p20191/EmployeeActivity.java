e22d2b572a1ff6b3ecd89a907bfbf9503ee8549d
yorgos <tsakirakii@gmail.com>
2024-07-29 17:28:16 +0300

    Added EmployeeActivity

---
+++ app/src/main/java/com/example/smartalert_p20191/EmployeeActivity.java
@@ -0,0 +1,14 @@
+package com.example.smartalert_p20191;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class EmployeeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_employee);
+    }
+}
