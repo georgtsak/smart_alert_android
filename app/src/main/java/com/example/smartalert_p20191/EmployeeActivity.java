0fb649d550f2d6374d7837883ba1fb32ab6a7655
yorgos <tsakirakii@gmail.com>
2024-08-08 23:55:24 +0300

    Added fragment for EmployeeActivity

--- app/src/main/java/com/example/smartalert_p20191/EmployeeActivity.java
+++ app/src/main/java/com/example/smartalert_p20191/EmployeeActivity.java
@@ -1,14 +1,49 @@
 package com.example.smartalert_p20191;
 
 import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class EmployeeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_employee);
+        setContentView(R.layout.activity_user);
+
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
+        // Set default fragment
+        if (savedInstanceState == null) {
+            loadFragment(new SubmissionFragment());
+        }
+
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            Fragment selectedFragment = null;
+            int itemId = item.getItemId();
+
+            if (itemId == R.id.employee)
+            {
+                selectedFragment = new SubmissionFragment();
+            } else if (itemId == R.id.statistics) {
+                selectedFragment = new StatisticsFragment();
+            } else if (itemId == R.id.settings) {
+                selectedFragment = new SettingsFragment();
+            }
+
+            return loadFragment(selectedFragment);
+        });
+    }
+    private boolean loadFragment(Fragment fragment) {
+        if (fragment != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.Fragment1, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
     }
 }
