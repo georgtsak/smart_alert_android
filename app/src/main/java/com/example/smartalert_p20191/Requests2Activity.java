b515a2838dc1a63bd23cd2860d14ec67b3a7f8c2
yorgos <tsakirakii@gmail.com>
2024-08-23 23:54:47 +0300

    fixed error:Glide

--- app/src/main/java/com/example/smartalert_p20191/Requests2Activity.java
+++ app/src/main/java/com/example/smartalert_p20191/Requests2Activity.java
@@ -9,6 +9,7 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.bumptech.glide.Glide;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
